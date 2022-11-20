8bfdacaa82e201e58572da590f4f72c881950d86
saif <you@example.com>
2022-11-20 19:54:08 +0600

    [synchronous] improve performance.

--- src/main/java/com/customerprocessor/ProcessingApplication.java
+++ src/main/java/com/customerprocessor/ProcessingApplication.java
@@ -5,6 +5,8 @@ import com.customerprocessor.action.Partition;
 
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class ProcessingApplication {
 
     public static void main(String[] args) {
@@ -12,6 +14,7 @@ public class ProcessingApplication {
         Scanner read = new Scanner(System.in);
         System.out.println("Enter 1 for insertion and 2 for file partition: ");
         while (read.hasNext()) {
+            System.out.println("============== Enter ===============");
             switch (read.nextInt()) {
                 case 1:
                     Insertion.start();
@@ -19,7 +22,11 @@ public class ProcessingApplication {
                 case 2:
                     Partition.start();
                     return;
+                default:
+                    exit(0);
             }
+
         }
+        System.out.println("============= completed. =================");
     }
 }
--- src/main/java/com/customerprocessor/processor/FileWriterExecutor.java
+++ src/main/java/com/customerprocessor/processor/FileWriterExecutor.java
@@ -19,41 +19,47 @@ public final class FileWriterExecutor extends AbstractExecutor {
 
     @Override
     public void run() {
-        var statement = getConnector().get().getStatement();
-        if (statement.isPresent()) {
-            int start = 0;
-            try {
+        synchronized (getConnector()) {
+            var statement = getConnector().get().getStatement();
+            if (statement.isPresent()) {
+                int start = 0;
+                try {
+                    while (start < 100000) {
+                        var resultSet = statement.get().executeQuery(query(start));
+                        var customers = new ArrayList<Customer>();
+                        while (resultSet.next()) {
+                            customers.add(Customer
+                                    .builder()
+                                    .firstName(resultSet.getString("first_name"))
+                                    .lastName(resultSet.getString("last_name"))
+                                    .city(resultSet.getString("city"))
+                                    .state(resultSet.getString("state"))
+                                    .postalZipCode(resultSet.getString("postal_zip_code"))
+                                    .phoneNumber(resultSet.getString("phone_number"))
+                                    .email(resultSet.getString("email"))
+                                    .ipAddress(resultSet.getString("ip_address"))
+                                    .build());
 
-                while (start < 100000) {
-                    var resultSet = statement.get().executeQuery(query(start));
-                    var customers = new ArrayList<Customer>();
-                    while (resultSet.next()) {
-                        customers.add(Customer
-                                .builder()
-                                .firstName(resultSet.getString("first_name"))
-                                .lastName(resultSet.getString("last_name"))
-                                .city(resultSet.getString("city"))
-                                .state(resultSet.getString("state"))
-                                .postalZipCode(resultSet.getString("postal_zip_code"))
-                                .phoneNumber(resultSet.getString("phone_number"))
-                                .email(resultSet.getString("email"))
-                                .ipAddress(resultSet.getString("ip_address"))
-                                .build());
-
+                        }
+                        start += FILE_WRITE_PARTITION_SIZE;
+                        if (customers.isEmpty()) {
+                            break;
+                        }
+                        getRunner().add(new WriterProcessor(customers, getCustomerType()));
                     }
-                    start += FILE_WRITE_PARTITION_SIZE;
-                    if(customers.isEmpty()){
-                        break;
-                    }
-                    getRunner().add(new WriterProcessor(customers, getCustomerType()));
+                    statement.get().close();
+                } catch (SQLException exception) {
+                    throw new ApplicationException(exception);
                 }
+            }
+            parallelism();
+            getLOGGER().info("Submitted all Threads");
+            try {
                 statement.get().close();
             } catch (SQLException exception) {
-                throw new ApplicationException(exception);
+                getLOGGER().error(exception);
             }
+            getConnector().get().close();
         }
-        parallelism();
-        getLOGGER().info("Submitted all Threads");
-        this.stop();
     }
 }
