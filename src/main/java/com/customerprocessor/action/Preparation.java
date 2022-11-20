74b2bd5778034df6cefe80b1a4853200771e1172
saif <you@example.com>
2022-11-20 20:06:39 +0600

    [block] is removed.

--- src/main/java/com/customerprocessor/action/Preparation.java
+++ src/main/java/com/customerprocessor/action/Preparation.java
@@ -1,11 +1,8 @@
 package com.customerprocessor.action;
 
 
-import com.customerprocessor.exception.ApplicationException;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class Preparation {
     private static final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
@@ -21,11 +18,5 @@ public class Preparation {
 
     public void shutdown() {
         pool.shutdown();
-        try {
-            while (pool.awaitTermination(10, TimeUnit.SECONDS));
-            pool.shutdownNow();
-        } catch (InterruptedException exception) {
-            throw new ApplicationException(exception);
-        }
     }
 }
